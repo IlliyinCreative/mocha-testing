eeb4163e9711e03db72bef752af05f06e5c1b5ce
picfubri <picfubri@gmail.com>
2021-02-13 23:33:48 +0700

    add

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-module.exports = function () {
+module.exports = {
+    sayHello: function () {
+        return 'hello';
+    },
+    addNumbers: function (value1, value2) {
+        return value1 + value2;
+    }
+}
+
+/*module.exports = function () {
     return 'hello';
-}; // u can add anything or u can fix the problem
+};*/ // u can add anything or u can fix the problem
